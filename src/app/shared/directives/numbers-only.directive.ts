29f6a20d5ec31ea6d01fa4ef2079082e0789f914
George Kheladze <gkh@Georges-MacBook-Air.local>
2023-03-31 13:18:01 +0400

    bug fix/numbers-only.directive

--- src/app/shared/directives/numbers-only.directive.ts
+++ src/app/shared/directives/numbers-only.directive.ts
@@ -7,8 +7,10 @@ export class NumbersOnlyDirective {
   constructor() {}
 
   @HostListener('input', ['$event']) onInputChange(event: any) {
-    let inputValue = event.target.value.replace(/\s+/g, '');
+    let inputValue = event.target.value;
+    inputValue = inputValue.replace(/\D/g, '');
     inputValue = inputValue.replace(/(\d{3})(?=\d)/g, '$1 ');
     event.target.value = inputValue;
   }
+
 }
